b342b25d54e052787b52cebfac39beac2eac28b9
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2023-09-11 12:24:24 +0400

    finished megaphone

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 	else if (argc > 1) {
 		for (int i = 1; argv[i]; i++)
 			for (int j = 0; argv[i][j]; j++)
-				putchar(toupper(argv[i][j]));
+				std::cout << (char) toupper(argv[i][j]);
 		std::cout << std::endl;
 	}
 	return 0;
