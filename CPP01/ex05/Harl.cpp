d65d4180b678e5c59c2174c3a3fae91597183386
Tanvir <tanvir@laptop>
2023-09-30 00:48:18 +0400

    completed CPP01:ex05

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,48 @@
+#include "Harl.hpp"
+
+Harl::Harl(void) {
+	std::cout << "Harl has arrived" << std::endl;
+	return ;
+}
+
+Harl::~Harl(void) {
+	std::cout << "Harl has left" << std::endl;
+	return ;
+}
+
+void Harl::debug(void) {
+	std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	return ;
+}
+
+void Harl::info(void) {
+	std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	return ;
+}
+
+void Harl::warning(void) {
+	std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	return ;
+}
+
+void Harl::error(void) {
+	std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now." << std::endl;
+	return ;
+}
+
+void Harl::complain(std::string level) {
+	functionPtr pointers[4] = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			(this->*pointers[i])();
+			return ;
+		}
+	}
+	std::cout << "Invalid level was entered." << std::endl;
+}
