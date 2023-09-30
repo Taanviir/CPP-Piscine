ee2e86df6298d34db6af02cb9352b6cf363e9de6
Tanvir <tanvir@laptop>
2023-09-30 13:47:14 +0400

    completed CPP02:ex00

---
+++ CPP02/ex00/Fixed.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) {
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixedPointValue = 0;
+}
+
+Fixed::Fixed(const Fixed& src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_fixedPointValue = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedPointValue = raw;
+}
