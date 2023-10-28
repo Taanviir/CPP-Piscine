25ebfab0ba5fd2c285c12fa1a93a2b23748bf57a
Tanvir <tanvir@laptop>
2023-10-28 13:45:32 +0400

    fix: CPP02/ex00 changed how copy constructors work

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,20 +1,24 @@
 #include "Fixed.hpp"
+#include <iostream>
+
+const int Fixed::_fractionalBits = 8;
 
 Fixed::Fixed(): _fixedPointValue(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& src): _fixedPointValue(src._fixedPointValue) {
+Fixed::Fixed(const Fixed& copy) {
 	std::cout << "Copy constructor called" << std::endl;
+	this->_fixedPointValue = copy.getRawBits();
 }
 
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed& Fixed::operator=(const Fixed& rhs) {
+Fixed& Fixed::operator=(const Fixed& copy) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->_fixedPointValue = rhs.getRawBits();
+	this->_fixedPointValue = copy.getRawBits();
 	return *this;
 }
 
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -1,24 +1,20 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
-#include <iostream>
-
 class Fixed {
-
 public:
 	Fixed();
-	Fixed(const Fixed& src);
+	Fixed(const Fixed& copy);
 	~Fixed();
 
-	Fixed& operator=(const Fixed& rhs);
+	Fixed& operator=(const Fixed& copy);
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 
 private:
 	int _fixedPointValue;
-	static const int _fractionalBits = 8;
-
+	static const int _fractionalBits;
 };
 
 #endif
