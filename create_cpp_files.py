cac78d50f860ff59724509567953ce92608695a3
Tanvir <tanvir@laptop>
2023-11-12 01:18:16 +0400

    feat: script creats makefile  now

--- create_cpp_files.py
+++ create_cpp_files.py
@@ -34,14 +34,50 @@ def generate_makefile(output_path="./"):
     else:
         file_path = "Makefile"
 
-    with open('./Makefile_template', 'r') as template_file:
-        makefile_contents = template_file.read()
-    
     execname = input("Enter the name of the executable: ")
-    makefile_contents = makefile_contents.replace("<program_name>", execname)
 
     with open(file_path, 'w') as makefile:
-        makefile.write(makefile_contents)
+        makefile.write("### COLORS\n")
+        makefile.write("GREEN_B = \"\\033[1;32m\"\n")
+        makefile.write("RED_B = \"\\033[1;31m\"\n")
+        makefile.write("BLUE_I = \"\\033[2;34m\"\n")
+        makefile.write("RESET = \"\\033[0m\"\n\n")
+        
+        makefile.write(f"NAME = {execname}\n")
+        makefile.write("CPPFLAGS = -Wall -Werror -Wextra -std=c++98\n\n")
+        
+        makefile.write("SRCS = $(wildcard *.cpp)\n\n")
+        
+        makefile.write("OBJ_DIR = obj/\n")
+        makefile.write("OBJS_LIST = $(SRCS:.cpp=.o)\n")
+        makefile.write("OBJS = $(addprefix $(OBJ_DIR), $(OBJS_LIST))\n\n")
+        
+        makefile.write("all : $(NAME)\n\n")
+        
+        makefile.write("$(NAME) : $(OBJS)\n")
+        makefile.write("\t@c++ $(CPPFLAGS) $(OBJS) -o $@\n")
+        makefile.write("\t@echo $(GREEN_B)$(NAME)$(RESET)\" is ready. ✅\\n\"\n\n")
+        
+        makefile.write("$(OBJ_DIR)%.o : %.cpp\n")
+        makefile.write("\t@mkdir -p $(OBJ_DIR)\n")
+        makefile.write("\t@c++ $(CPPFLAGS) -I./ -c $< -o $@\n")
+        makefile.write("\t@echo $(BLUE_I)\"Compiling $<.\"$(RESET)\n\n")
+        
+        makefile.write("valgrind : CPPFLAGS+=-g\n")
+        makefile.write("valgrind : re\n")
+        makefile.write("\t@valgrind --leak-check=full --show-leak-kinds=all ./$(NAME)\n\n")
+        
+        makefile.write("clean :\n")
+        makefile.write("\t@rm -rf $(OBJ_DIR)\n")
+        makefile.write("\t@echo $(RED_B)\"Object files removed.\"$(RESET)\n\n")
+        
+        makefile.write("fclean : clean\n")
+        makefile.write("\t@rm -f $(NAME)\n")
+        makefile.write("\t@echo $(RED_B)\"$(NAME) removed.\\n\"$(RESET)\n\n")
+        
+        makefile.write("re : fclean all\n\n")
+        
+        makefile.write(".PHONY : all clean fclean re\n")
 
 def main():
     if len(sys.argv) < 2:
