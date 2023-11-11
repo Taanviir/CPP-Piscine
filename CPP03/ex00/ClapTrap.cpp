34dab0b66c4f6d73bc5d983b8f1dd74ba0179b69
Tanvir <tanvir@laptop>
2023-11-11 14:13:16 +0400

    feat: changed nameless claptrap to default claptrap added checks for methods

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -1,12 +1,14 @@
 #include "ClapTrap.hpp"
 #include <cstring>
+#include <iostream>
 
-ClapTrap::ClapTrap(): _name("") {
-	std::cout << "Default constructor called for nameless ClapTrap" << std::endl;
+ClapTrap::ClapTrap(): _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+	std::cout << "Default constructor called" << std::endl;
 }
 
-ClapTrap::ClapTrap(const ClapTrap& copy): _name(copy._name), _hitPoints(copy._hitPoints), _energyPoints(copy._energyPoints), _attackDamage(copy._attackDamage) {
+ClapTrap::ClapTrap(const ClapTrap& copy) {
 	std::cout << "Copy constructor called for ClapTrap " << _name << std::endl;
+	*this = copy;
 }
 
 ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
@@ -14,10 +16,7 @@ ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints
 }
 
 ClapTrap::~ClapTrap() {
-	if (_name == "")
-		std::cout << "Destructor called for nameless ClapTrap" << std::endl;
-	else
-		std::cout << "Destructor called for ClapTrap " << _name << std::endl;
+	std::cout << "Destructor called for ClapTrap " << _name << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& copy) {
@@ -32,36 +31,43 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& copy) {
 }
 
 void ClapTrap::attack(const std::string& target) {
-	if (this->_energyPoints == 0) {
-		std::cout << "ClapTrap " << this->_name << " cannot attack due to insufficient energy points. 😴" << std::endl;
+	if (_energyPoints == 0) {
+		std::cout << "ClapTrap " << _name << " cannot attack due to insufficient energy points. 😴" << std::endl;
 		return;
-	} else if (this->_hitPoints == 0) {
-		std::cout << "ClapTrap " << this->_name << " has fallen. ☠️" << std::endl;
+	} else if (_hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " has fallen. ☠️" << std::endl;
 		return;
 	}
 
-	std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage! ⚔️" << std::endl;
-	this->_energyPoints--;
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage! ⚔️" << std::endl;
+	_energyPoints--;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
-	std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage. 🤕" << std::endl;
-	this->_hitPoints -= amount;
+	if (_hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " is already dead, stop attacking!" << std::endl;
+		return;
+	} else if (_hitPoints > amount) {
+		_hitPoints -= amount;
+	} else if (_hitPoints <= amount) {
+		_hitPoints = 0;
+	}
+	std::cout << "ClapTrap " << _name << " takes " << amount << " points of damage. 🤕" << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-	if (this->_energyPoints == 0) {
-		std::cout << "ClapTrap " << this->_name << " cannot attack due to insufficient energy points. 😴" << std::endl;
+	if (_energyPoints == 0) {
+		std::cout << "ClapTrap " << _name << " cannot attack due to insufficient energy points. 😴" << std::endl;
 		return;
-	} else if (this->_hitPoints == 0) {
-		std::cout << "ClapTrap " << this->_name << " has already fallen. ☠️" << std::endl;
+	} else if (_hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " has already fallen. ☠️" << std::endl;
 		return;
-	} else if ((this->_hitPoints + amount) > 10) {
-		std::cout << "ClapTrap " << this->_name << " cannot have its hit points exceed 10." << std::endl;
+	} else if ((_hitPoints + amount) > 10) {
+		std::cout << "ClapTrap " << _name << " cannot have its hit points exceed 10." << std::endl;
 		return;
 	}
 
-	std::cout << "ClapTrap " << this->_name << " repairs itself with " << amount << " hit points. 🛠️" << std::endl;
-	this->_hitPoints += amount;
-	this->_energyPoints--;
+	std::cout << "ClapTrap " << _name << " repairs itself with " << amount << " hit points. 🛠️" << std::endl;
+	_hitPoints += amount;
+	_energyPoints--;
 }
