a7160165bf642290c950a4687ebc9af219f33217
Tanvir <tanvir@laptop>
2023-12-17 11:55:23 +0400

    feat: started bsp in CPP02

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -1,5 +1,26 @@
 #include "Point.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point) {
-	
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+	// Vertices of the triangle
+	Fixed Ax = a.getX(), Ay = a.getY();
+	std::cout << "Ax: " << Ax << " Ay: " << Ay << std::endl;
+	Fixed Bx = b.getX(), By = b.getY();
+	std::cout << "Bx: " << Bx << " By: " << By << std::endl;
+	Fixed Cx = c.getX(), Cy = c.getY();
+	std::cout << "Cx: " << Cx << " Cy: " << Cy << std::endl;
+
+	// Point to be found
+	Fixed Px = point.getX(), Py = point.getY();
+	std::cout << "Px: " << Px << " Py: " << Py << std::endl;
+	Fixed numerator = (Ax * (Cy - Ay)) + (Py - Ay) * (Cx - Ax) - Px * (Cy - Ay);
+	Fixed denominator = (By - Ay) * (Cx - Ax) - (Bx - Ax) * (Cy - Ay);
+	Fixed length1(numerator / denominator);
+
+	numerator = Py - Ay - length1 * (By - Ay);
+	denominator = Cy - Ay;
+	Fixed length2(numerator / denominator);
+
+	if (length1 >= 0 && length2 >= 0 && (length1 + length2) <= 1)
+		return true;
+	return false;
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -1,5 +1,17 @@
-#include "Fixed.hpp"
+#include "Point.hpp"
 
 int main(void) {
+	// need a test case to see if the point is inside the triangle
+	
+	Point a(1, 1);
+	Point b(2, 5);
+	Point c(5, 3);
+
+	Point point(2, 2);
+
+	if (bsp(a, b, c, point))
+		std::cout << "Point is inside the triangle" << std::endl;
+	else
+		std::cout << "Point is outside the triangle" << std::endl;
 	return 0;
 }
