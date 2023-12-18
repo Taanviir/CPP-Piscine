231c29a08f2614ddfab7d66ea22424a2e78e3091
Tanvir <tanvir@laptop>
2023-12-18 11:39:34 +0400

    test: added more test cases for CPP02/ex02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -15,5 +15,35 @@ int main(void) {
 
 	std::cout << Fixed::max( a, b ) << std::endl;
 
+	// Testing conditionals
+	Fixed c(5);
+	Fixed d(5);
+	std::cout << "c = " << c << std::endl;
+	std::cout << "d = " << d << std::endl;
+	std::cout << "c == d: " << (c == d) << std::endl;
+	std::cout << "c != d: " << (c != d) << std::endl;
+	std::cout << "c > d: " << (c > d) << std::endl;
+	std::cout << "c < d: " << (c < d) << std::endl;
+	std::cout << "c >= d: " << (c >= d) << std::endl;
+	std::cout << "c <= d: " << (c <= d) << std::endl;
+
+	// Testing arithmetic
+	Fixed e(5);
+	Fixed f(2);
+	std::cout << "e = " << e << std::endl;
+	std::cout << "f = " << f << std::endl;
+	std::cout << "e + f = " << (e + f) << std::endl;
+	std::cout << "e - f = " << (e - f) << std::endl;
+	std::cout << "e * f = " << (e * f) << std::endl;
+	std::cout << "e / f = " << (e / f) << std::endl;
+	
+	// Testing min/max
+	Fixed g(5);
+	Fixed h(2);
+	std::cout << "g = " << g << std::endl;
+	std::cout << "h = " << h << std::endl;
+	std::cout << "min(g, h) = " << Fixed::min(g, h) << std::endl;
+	std::cout << "max(g, h) = " << Fixed::max(g, h) << std::endl;
+
 	return 0;
 }
