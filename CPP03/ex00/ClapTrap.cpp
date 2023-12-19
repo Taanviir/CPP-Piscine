e04054b1789c35f5dd0488bc63fc2d7bc9b77386
Tanvir <tanvir@laptop>
2023-12-19 12:08:43 +0400

    style: cleaned up ClapTrap in CPP03/ex00

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,16 +6,16 @@ ClapTrap::ClapTrap(): _name("Default"), _hitPoints(10), _energyPoints(10), _atta
 	std::cout << "Default constructor called for ClapTrap" << std::endl;
 }
 
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called for ClapTrap " << _name << std::endl;
+}
+
 ClapTrap::ClapTrap(const ClapTrap& copy): _name(copy._name), _hitPoints(copy._hitPoints), _energyPoints(copy._energyPoints), _attackDamage(copy._attackDamage) {
 	std::cout << "Copy constructor called for ClapTrap " << _name << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
 	std::cout << "Constructor called for ClapTrap " << _name << std::endl;
-}
-
-ClapTrap::~ClapTrap() {
-	std::cout << "Destructor called for ClapTrap " << _name << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& copy) {
