4379ec6bee6416346d51c61644c4d8c770ce2959
Tanvir <tanvir@laptop>
2023-12-22 19:33:03 +0400

    style(CPP04/ex01): Cleaned up

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -2,40 +2,43 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int main()
+int main(void)
 {
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
 
-    delete i;
-    delete j; //should not create a leak
-    std::cout << std::endl << std::endl;
+	delete i;
+	delete j; //should not create a leak
+	std::cout << std::endl;
 
-    // proof of deep copy
-    Dog dog1;
-    std::cout << "Dog1: ";
-    dog1.makeSound();
-    std::cout << "\nDog1 Type: " << dog1.getType() << std::endl;
-    // new dog
-    Dog dog2 = dog1;
-    std::cout << "\nDog2 Type: " << dog2.getType() << std::endl;
-    // modifying dog1's type
-    dog1.setType("Modified Dog");
-    // final results
-    std::cout << "\nDog1 New Type: " << dog1.getType() << std::endl;
-    std::cout << "\nDog2 type: " << dog2.getType() << std::endl << std::endl;
+	// proof of deep copy
+	Dog dog1;
+	std::cout << "Dog1: ";
+	dog1.makeSound();
+	std::cout << "Dog1 Type: " << dog1.getType() << std::endl;
 
-    // array of animals, half dogs, half cats
-    const Animal *animals[4];
-    for (int i = 0; i < 4; i++) {
-        if (i < 10)
-            animals[i] = new Dog();
-        else
-            animals[i] = new Cat();
-    }
-    for (int i = 0; i < 4; i++) {
-        delete animals[i];
-    }
+	// new dog
+	Dog dog2 = dog1;
+	std::cout << "Dog2 Type: " << dog2.getType() << std::endl;
 
-    return 0;
+	// modifying dog1's type
+	dog1.setType("Modified Dog");
+
+	// final results
+	std::cout << "Dog1 New Type: " << dog1.getType() << std::endl;
+	std::cout << "Dog2 type: " << dog2.getType() << std::endl << std::endl;
+
+	// array of animals, half dogs, half cats
+	const Animal *animals[4];
+	for (int i = 0; i < 4; i++) {
+		if (i < 10)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
+	}
+	for (int i = 0; i < 4; i++) {
+		delete animals[i];
+	}
+
+	return 0;
 }
