5744b4cf92d66c2ed78690d7be0bbe43a5c340cc
Tanvir <tanvir@laptop>
2023-12-23 22:20:16 +0400

    feat(CPP04/ex03): Added print statements to AMateria class

--- CPP04/ex03/AMateria.cpp
+++ CPP04/ex03/AMateria.cpp
@@ -2,25 +2,31 @@
 #include <iostream>
 
 AMateria::AMateria() {
+	std::cout << "AMateria default constructor called" << std::endl;
 	_type = "No Type";
 }
 
-AMateria::~AMateria() {}
+AMateria::~AMateria() {
+	std::cout << "AMateria destructor called" << std::endl;
+}
 
 AMateria::AMateria(const AMateria& copy) {
+	std::cout << "AMateria copy constructor called" << std::endl;
 	this->_type = copy._type;
 }
 
 AMateria::AMateria(std::string const* type) {
 	if (!type || (*type).empty()) {
-		std::cout << "Invalid type entered, type set to \"No Type\"" << std::endl;
+		std::cout << "AMateria constructor called for Invalid type, type set to \"No Type\"" << std::endl;
 		_type = "No Type";
 		return;
 	}
+	std::cout << "AMateria constructor called for type " << *type << std::endl;
 	this->_type = *type;
 }
 
 AMateria::AMateria(std::string const& type) {
+	std::cout << "AMateria constructor called for " << type << std::endl;
 	if (type.empty()) {
 		std::cout << "Invalid type entered, type set to \"No Type\"" << std::endl;
 		_type = "No Type";
@@ -30,6 +36,7 @@ AMateria::AMateria(std::string const& type) {
 }
 
 AMateria& AMateria::operator=(const AMateria& copy) {
+	std::cout << "AMateria assignment operator called" << std::endl;
 	this->_type = copy._type;
 	return *this;
 }
@@ -39,5 +46,5 @@ std::string const& AMateria::getType() const {
 }
 
 void AMateria::use(ICharacter& target) {
-
+	std::cout << "* abstract use on target " << target.getName() << " *" << std::endl;
 }
