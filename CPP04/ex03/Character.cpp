8f14388c3bb0b32fc4afe240ecfaf3711414698e
Tanvir <tanvir@laptop>
2023-12-24 02:07:36 +0400

    feat(CPP04/ex03): Implemented Character class

--- CPP04/ex03/Character.cpp
+++ CPP04/ex03/Character.cpp
@@ -1,2 +1,88 @@
 #include "Character.hpp"
+#include <iostream>
 
+Character::Character() {
+	std::cout << "Character default constructor called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		_materias[i] = NULL;
+	}
+}
+
+Character::~Character() {
+	std::cout << "Character destructor called" << std::endl;
+}
+
+Character::Character(const Character& copy) {
+	std::cout << "Character copy constructor called" << std::endl;
+	this->_name = copy._name;
+}
+
+Character::Character(std::string const* name) {
+	if (!name || (*name).empty()) {
+		std::cout << "Character constructor called for Invalid name, name set to \"No Name\"" << std::endl;
+		_name = "No Name";
+		return;
+	}
+	std::cout << "Character constructor called for name " << *name << std::endl;
+	this->_name = *name;
+}
+
+Character::Character(std::string const& name) {
+	std::cout << "Character constructor called for " << name << std::endl;
+	if (name.empty()) {
+		std::cout << "Invalid name entered, name set to \"No Name\"" << std::endl;
+		_name = "No Name";
+		return;
+	}
+	this->_name = name;
+}
+
+Character& Character::operator=(const Character& copy) {
+	std::cout << "Character assignment operator called" << std::endl;
+	this->_name = copy._name;
+	return *this;
+}
+
+std::string const& Character::getName() const {
+	return _name;
+}
+
+void Character::equip(AMateria* m) {
+	if (!m) {
+		std::cout << "Invalid materia entered!" << std::endl;
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (!_materias[i]) {
+			_materias[i] = m;
+			std::cout << "Materia " << m->getType() << " equipped" << std::endl;
+			return;
+		}
+	}
+	std::cout << "No empty slots available" << std::endl;
+}
+
+void Character::unequip(int idx) {
+	if (idx < 0 || idx > 3) {
+		std::cout << "Invalid index entered!" << std::endl;
+		return;
+	}
+	if (!_materias[idx]) {
+		std::cout << "No materia equipped at index " << idx << std::endl;
+		return;
+	}
+	_materias[idx] = NULL;
+	std::cout << "Materia at index " << idx << " unequipped" << std::endl;
+}
+
+void Character::use(int idx, ICharacter& target) {
+	if (idx < 0 || idx > 3) {
+		std::cout << "Invalid index entered!" << std::endl;
+		return;
+	}
+	if (!_materias[idx]) {
+		std::cout << "No materia equipped at index " << idx << std::endl;
+		return;
+	}
+	_materias[idx]->use(target);
+}
--- CPP04/ex03/Character.hpp
+++ CPP04/ex03/Character.hpp
@@ -1,18 +1,27 @@
 #ifndef CHARACTER_HPP
 # define CHARACTER_HPP
 
-#include "AMateria.hpp"
+# include "AMateria.hpp"
+# include "ICharacter.hpp"
 
-class Character {
+class Character: public ICharacter {
 public:
 	Character();
 	~Character();
 	Character(const Character& copy);
+	Character(std::string const* name);
+	Character(std::string const& name);
 
 	Character& operator=(const Character& copy);
 
+	std::string const& getName() const;
+	void equip(AMateria* m);
+	void unequip(int idx);
+	void use(int idx, ICharacter& target);
+
 private:
-	// AMateria _materias[4];
+	std::string _name;
+	AMateria* _materias[4];
 
 };
 
