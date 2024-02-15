2bf041c96934910fe53b6eda323b71e87ef1e89f
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-15 17:21:06 +0400

    feat(CPP07/ex00): implemented min,max,swap templates

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,21 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T& value1, T& value2) {
+    T temp = value1;
+    value1 = value2;
+    value2 = temp;
+}
+
+template <typename T>
+const T& min(const T& value1, const T& value2) {
+    return value1 < value2 ? value1 : value2;
+}
+
+template <typename T>
+const T& max(const T& value1, const T& value2) {
+    return value1 > value2 ? value1 : value2;
+}
+
+#endif // WHATEVER_HPP
