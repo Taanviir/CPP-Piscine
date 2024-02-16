f28a34c6e7187e5ac13ee8b91c4da8c5a1ffede6
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-17 02:02:54 +0400

    feat(CPP08/ex02): Implemented MutantStack iterators

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,13 +1,25 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-#include <deque>
 #include <stack>
 
 template < typename T >
 class MutantStack: public std::stack<T> {
 public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
 
+    MutantStack() {}
+    MutantStack(MutantStack const & src) { *this = src; }
+    ~MutantStack() {}
+    MutantStack & operator=(MutantStack const & src) {
+        if (this != &src) {
+            std::stack<T>::operator=(src);
+        }
+        return *this;
+    }
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
 
 private:
 
