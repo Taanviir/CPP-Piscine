80b631cacf6bfea555b16942412035d4808a9299
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-17 01:40:42 +0400

    feat(CPP08/ex02): Implementing MutantStack

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <deque>
+#include <stack>
+
+template < typename T >
+class MutantStack: public std::stack<T> {
+public:
+
+
+private:
+
+};
+
+#endif // MUTANTSTACK_HPP
