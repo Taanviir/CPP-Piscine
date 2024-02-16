5601634bb0af27ab0f4ff247a0ffad2fb07bbffe
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-17 02:18:38 +0400

    feat(CPP09/ex00): setup BitcoinExchange

---
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,18 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {
+
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const& copy) {
+
+}
+
+BitcoinExchange::~BitcoinExchange() {
+
+}
+
+BitcoinExchange& BitcoinExchange::operator=(BitcoinExchange const& copy) {
+    return *this;
+}
+
---
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,14 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange {
+public:
+    BitcoinExchange();
+    BitcoinExchange(BitcoinExchange const& copy);
+    ~BitcoinExchange();
+    BitcoinExchange &operator=(BitcoinExchange const& copy);
+
+private:
+};
+
+#endif // BITCOINEXCHANGE_HPP
