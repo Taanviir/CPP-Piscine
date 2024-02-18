fb57ec95b38bfa70e4f8b0b5a3eba518aed991ae
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-19 01:12:42 +0400

    feat(CPP09/ex00): Implemented BitcoinExchange()

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -1,15 +1,32 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange() {
+    // Reading the exchange rates from the data.csv file
+    std::ifstream dataFile(DATA_FILE);
+    if (!dataFile.is_open())
+        throw std::runtime_error("Error: could not open data.csv file");
 
+    std::string line;
+    while (getline(dataFile, line)) {
+        if (line.empty() || line.compare("date,exchange_rate") == 0)
+            continue;
+        
+        size_t pos = line.find(',');
+        if (pos == std::string::npos)
+            throw std::runtime_error("Error: bad data format");
+        
+        std::string date = line.substr(0, pos);
+        double rate = strtod(line.substr(pos + 1).c_str(), NULL);
+        std::pair<std::string, double> entry(date, rate);
+        _exchangeRates.insert(entry);
+    }
+    dataFile.close();
 }
+
+BitcoinExchange::~BitcoinExchange() {}
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const& copy) {
     (void) copy;
-}
-
-BitcoinExchange::~BitcoinExchange() {
-
 }
 
 BitcoinExchange& BitcoinExchange::operator=(BitcoinExchange const& copy) {
