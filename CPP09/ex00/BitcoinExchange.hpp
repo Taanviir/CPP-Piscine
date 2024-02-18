27174d2e5e1240268dbabfb9e132c5e73e5b58eb
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-19 01:47:40 +0400

    chore(CPP09/ex00): Cleaned up header file

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -8,14 +8,17 @@
 # include <iostream>
 # include <cstdlib>
 # include <sstream>
+# include <iomanip>
 
 // Error messages
-#define ERROR_DATE_FORMAT "Error: bad input"
-#define ERROR_MISSING_VALUE "Error: value is missing."
-#define ERROR_LARGE "Error: value is too large."
-#define ERROR_NEGATIVE "Error: value is not positive."
+# define ERROR_BAD_DATE "Error: bad input"
+# define ERROR_MISSING_DATA "Error: missing date."
+# define ERROR_MISSING_VALUE "Error: missing value."
+# define ERROR_MISSING_DEL "Error: missing delimiter."
+# define ERROR_LARGE "Error: value is too large."
+# define ERROR_NEGATIVE "Error: not a positive number."
 
-#define DATA_FILE "./data.csv"
+# define DATA_FILE "./data.csv"
 
 class BitcoinExchange {
 public:
