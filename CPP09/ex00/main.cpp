6d97888e0312502621e84e65eb6185d22524fe45
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-19 01:13:56 +0400

    feat(CPP09/ex00): Updated main and started parsing

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include "BitcoinExchange.hpp"
+#include <sys/stat.h>
 
-int main() {
-    // Your code here
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Usage: ./btc [input file]" << std::endl;
+        return 1;
+    }
+
+    // Checking if the input file exists and is not a directory
+    struct stat fileInfo;
+    stat(argv[1], &fileInfo);
+    if (S_ISDIR(fileInfo.st_mode)) {
+        std::cout << "Error: " << argv[1] << " is a directory" << std::endl;
+        return 1;
+    }
+
+    // Checking if the input file cannot be opened or is empty
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cout << "Error: could not open " << argv[1] << std::endl;
+        return 1;
+    }
+    if (inputFile.peek() == std::ifstream::traits_type::eof()) {
+        std::cout << "Error: " << argv[1] << " is empty" << std::endl;
+        return 1;
+    }
+
+    // Reading the input file
+    std::string line;
+    
+
+    BitcoinExchange btc;
+
     return 0;
 }
