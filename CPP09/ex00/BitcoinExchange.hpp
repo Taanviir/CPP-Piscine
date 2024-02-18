8a7b153abc2a950b3a947d480bece8bfca74a65f
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-19 01:12:04 +0400

    feat(CPP09/ex00): Cleaned up header file and added convertAmount()

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -1,14 +1,34 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <string>
+# include <map>
+# include <vector>
+# include <fstream>
+# include <iostream>
+# include <cstdlib>
+# include <sstream>
+
+// Error messages
+#define ERROR_DATE_FORMAT "Error: bad input"
+#define ERROR_MISSING_VALUE "Error: value is missing."
+#define ERROR_LARGE "Error: value is too large."
+#define ERROR_NEGATIVE "Error: value is not positive."
+
+#define DATA_FILE "./data.csv"
+
 class BitcoinExchange {
 public:
     BitcoinExchange();
-    BitcoinExchange(BitcoinExchange const& copy);
     ~BitcoinExchange();
-    BitcoinExchange &operator=(BitcoinExchange const& copy);
+
+    double convertAmount(const std::string& date, double amount);
 
 private:
+    std::map<std::string, double> _exchangeRates;
+
+    BitcoinExchange(BitcoinExchange const& copy);
+    BitcoinExchange &operator=(BitcoinExchange const& copy);
 };
 
 #endif // BITCOINEXCHANGE_HPP
