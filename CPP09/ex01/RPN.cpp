b3d8d93012cd589ac24cd38e0296395d96a898d1
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-02-19 15:27:49 +0400

    feat(CPP09/ex01): started ex01

---
+++ CPP09/ex01/RPN.cpp
@@ -0,0 +1,18 @@
+#include "RPN.hpp"
+
+RPN::RPN() {
+
+}
+
+RPN::RPN(RPN const& copy) {
+
+}
+
+RPN::~RPN() {
+
+}
+
+RPN& RPN::operator=(RPN const& copy) {
+    return *this;
+}
+
---
+++ CPP09/ex01/RPN.hpp
@@ -0,0 +1,14 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+class RPN {
+public:
+    RPN();
+    RPN(RPN const& copy);
+    ~RPN();
+    RPN &operator=(RPN const& copy);
+
+private:
+};
+
+#endif // RPN_HPP
