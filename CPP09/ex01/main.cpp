bc3a4a7a99559166d6482be06d0c634453b30c4e
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-02-19 15:33:15 +0400

    feat(CPP09/ex01): Added argc handling in main

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "RPN.hpp"
 
-int main() {
-    // Your code here
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Error: invalid arguments" << std::endl;
+        std::cout << "Usage: ./RPN [reversed Polish expression]" << std::endl;
+        return 1;
+    }
+    std::string expression = argv[1];
     return 0;
 }
