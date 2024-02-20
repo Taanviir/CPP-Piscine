bd8571ecc94ba7c16ae92fcb453a7b72660c2c1b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-21 01:40:26 +0400

    feat(CPP09/ex020: Added checks to program

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <limits>
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv) {
@@ -6,5 +9,31 @@ int main(int argc, char **argv) {
         std::cout << "Usage: ./PmergeMe [num1] [num2] ... [numN]" << std::endl;
         return 1;
     }
+
+    std::string numbers;
+    for (int i = 1; i < argc; i++) {
+        numbers += argv[i];
+        numbers += " ";
+    }
+    if (numbers.find("-") != std::string::npos) {
+        std::cout << "Error: Invalid input" << std::endl;
+        return 1;
+    }
+
+    int size = argc - 1;
+    int* array = new int[size];
+    for (int i = 1; i < argc; i++) {
+        double num = strtod(argv[i], NULL);
+        if (num < std::numeric_limits<int>::min() || num > std::numeric_limits<int>::max()) {
+            std::cout << "Error: Invalid input" << std::endl;
+            delete[] array;
+            return 1;
+        }
+        array[i - 1] = static_cast<int>(num);
+    }
+
+    PmergeMe pmergeMe(array, size);
+
+    delete [] array;
     return 0;
 }
