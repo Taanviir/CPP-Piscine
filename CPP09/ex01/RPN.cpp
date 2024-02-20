082b76ed9c78e836a4ebe58af5221cbbd041f77c
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-20 14:40:43 +0400

    chore(CPP09/ex01): cleaned up cpp file

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -1,16 +1,12 @@
 #include "RPN.hpp"
 
-RPN::RPN() {
-
-}
+RPN::RPN() {}
 
 RPN::RPN(RPN const& copy) {
     (void)copy;
 }
 
-RPN::~RPN() {
-
-}
+RPN::~RPN() {}
 
 RPN& RPN::operator=(RPN const& copy) {
     (void)copy;
