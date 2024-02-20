220f7da0c903c496c754b1affadff3eb558de7a6
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-20 14:41:14 +0400

    feat(CPP09/ex01): added RPN testing to main

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "RPN.hpp"
+#include <fstream>
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -6,6 +7,35 @@ int main(int argc, char **argv) {
         std::cout << "Usage: ./RPN [reversed Polish expression]" << std::endl;
         return 1;
     }
+
     std::string expression = argv[1];
+    if (expression.empty()) {
+        std::cout << "Error: invalid expression" << std::endl;
+        return 1;
+    }
+    try {
+        RPN::calculate(expression);
+    }
+    catch (std::exception &error) {
+        std::cout << error.what() << std::endl;
+        return 1;
+    }
+
+    #ifdef TEST
+    std::ifstream file("tests.txt");
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            try {
+                RPN::calculate(line);
+            }
+            catch (std::exception &error) {
+                std::cout << error.what() << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+    #endif
+
     return 0;
 }
